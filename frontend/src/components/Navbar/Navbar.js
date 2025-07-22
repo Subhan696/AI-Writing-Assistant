77a1cb0d2f81fe7c861857b5d13002ee8ee65925
Subhan Kashif <subhankashif696@gmail.com>
2025-07-22 15:57:25 +0500

    integrated backend and frontend

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,22 +4,36 @@ import { AuthContext } from '../../context/AuthContext';
 import './Navbar.css';
 
 const Navbar = () => {
-  const { isAuthenticated, logout } = useContext(AuthContext);
+  const { user, isAuthenticated, logout } = useContext(AuthContext);
 
   const authLinks = (
-    <ul>
-      <li>
-        <Link to="/">Editor</Link>
-      </li>
-      <li>
-        <Link to="/history">History</Link>
-      </li>
-      <li>
-        <a onClick={logout} href="#!">
-          Logout
-        </a>
-      </li>
-    </ul>
+    <div className="navbar-links">
+      <div className="user-status">
+        {user && (
+          <>
+            <span>{user.email}</span>
+            {user.isPro ? (
+              <span className="pro-badge">PRO</span>
+            ) : (
+              <span>{`Usage: ${user.dailyUsageCount} / 5`}</span>
+            )}
+          </>
+        )}
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Editor</Link>
+        </li>
+        <li>
+          <Link to="/history">History</Link>
+        </li>
+        <li>
+          <a onClick={logout} href="#!">
+            Logout
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 
   const guestLinks = (
